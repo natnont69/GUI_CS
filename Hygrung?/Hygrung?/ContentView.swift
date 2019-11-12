d9e5a47342dff707ea9f50bdef481c16d0988c27
Natchanon <aumauengintel@gmail.com>
2019-11-12 18:26:12 +0700

    Created File

---
+++ Hygrung?/Hygrung?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hygrung?
+//
+//  Created by Nont Natchanont on 12/11/2562 BE.
+//  Copyright © 2562 Nont Natchanont. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
